8a8ece37ecc08b335cd490c517ea22d38dcc0e2b
ArisaNozu <myshkin88@gmail.com>
2025-07-09 22:23:55 +0900

    usersのFactoryBotの記述を編集

---
+++ spec/factories/users.rb
@@ -0,0 +1,15 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {'test'}
+    email                 {'test@example.com'}
+    password              {'test1234'}
+    password_confirmation {password}
+    last_name             {'田中'}
+    first_name            {'太朗'}
+    last_name_kana        {'タナカ'}
+    first_name_kana       {'タロウ'}
+    birth_date            {'1990-01-01'}
+  end
+end
+
+
