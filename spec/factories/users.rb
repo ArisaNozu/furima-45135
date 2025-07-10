89cd56702150217ac89effa5e7ea83fd7b98e1ec
ArisaNozu <myshkin88@gmail.com>
2025-07-10 21:41:22 +0900

    Fakerを用いて毎回異なるメールアドレスを作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     nickname              {'test'}
-    email                 {'test@example.com'}
+    email                 { Faker::Internet.unique.email }
     password              {'test1234'}
     password_confirmation {password}
     last_name             {'田中'}
