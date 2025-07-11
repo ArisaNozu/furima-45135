1641351b663efef564f92410dbd20bd09f174170
ArisaNozu <myshkin88@gmail.com>
2025-07-11 18:30:41 +0900

    Active Storageの導入

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,6 +2,14 @@ class ProductsController < ApplicationController
 
   def index
   end
+  private
 
 
+
+
+  def product_params
+    params.require(:product).permit(:content, :image).merge(user_id: current_user.id)
+  end
+
+  
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -12,6 +12,7 @@ class Product < ApplicationRecord
 
 
   belongs_to :user
+  has_one_attached :image
 
 end
 
