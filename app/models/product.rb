bf4aa85e0f5d0286d30c44c4e83cd2f7633e7960
ArisaNozu <myshkin88@gmail.com>
2025-07-11 17:16:17 +0900

    productsモデルとテーブルを作成する

---
+++ app/models/product.rb
@@ -0,0 +1,19 @@
+class Product < ApplicationRecord
+
+
+  validates :name, presence: true                  
+  validates :description, presence: true           
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :shipping_cost_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :shipping_day_id, presence: true
+  validates :price, presence: true
+
+
+  belongs_to :user
+
+end
+
+
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_07_08_063705) do
+ActiveRecord::Schema[7.1].define(version: 2025_07_11_065727) do
+  create_table "products", charset: "utf8mb3", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description", null: false
+    t.integer "category_id", null: false
+    t.integer "condition_id", null: false
+    t.integer "shipping_cost_id", null: false
+    t.integer "prefecture_id", null: false
+    t.integer "shipping_day_id", null: false
+    t.integer "price", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_products_on_user_id"
+  end
+
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "last_name", null: false
@@ -29,4 +44,5 @@ ActiveRecord::Schema[7.1].define(version: 2025_07_08_063705) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "products", "users"
 end
