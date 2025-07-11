5b3137f15d81814266fb6716ef148b942babc10e
ArisaNozu <myshkin88@gmail.com>
2025-07-12 08:29:40 +0900

    productモデルにActive Hashのアソシエーション追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -14,6 +14,14 @@ class Product < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_cost
+  belongs_to :prefecture
+  belongs_to :shipping_day
+
+
 end
 
 
