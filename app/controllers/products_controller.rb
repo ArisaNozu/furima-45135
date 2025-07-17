dc8f831711be4df3746fcd0d24618839c6c5047e
ArisaNozu <myshkin88@gmail.com>
2025-07-17 21:29:35 +0900

    Productsコントローラーにログイン状態の場合にのみ、自身が出品した商品情報を削除できるよう設定

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
 
 
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :set_product, only: [:show, :edit, :update ,:destroy]
   before_action :move_to_index, only: [:edit, :update, :destroy]
 
