7042ca05c5f363d086bba674db9cfe34bcdf41b8
ArisaNozu <myshkin88@gmail.com>
2025-07-17 17:45:14 +0900

    Productコントローラーのupdate Action追加

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -34,6 +34,15 @@ class ProductsController < ApplicationController
   end
 
 
+  def update
+  @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to product_path(@product)
+    else
+      render :edit
+    end
+  end
+
 
   private
 
