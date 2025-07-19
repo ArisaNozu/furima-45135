8b688d700ae0c5133f6ad8fe9d0787aa6984bb4b
ArisaNozu <myshkin88@gmail.com>
2025-07-19 23:09:34 +0900

    oriderコントローラーの未ログインでの遷移・自身の商品の購入不可

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,24 +1,39 @@
 class OrdersController < ApplicationController
-
+  before_action :authenticate_user!
   before_action :set_product
+  before_action :move_to_root, only: [:index, :create]
 
   def index
     @order_form = OrderForm.new
   end
 
   def create
-  @order_form = OrderForm.new(order_form_params)
+  @order_form = OrderForm.new(order_form_params.merge(user_id: current_user.id, product_id: @product.id))
 
     if @order_form.valid?
       @order_form.save
       redirect_to root_path
     else
-      render :new
+      render :index
     end
   end
 
   def set_product
     @product = Product.find(params[:product_id])
+  end
+
+    def move_to_root
+    # 自分が出品者、またはすでに購入済みの商品だったらトップに戻す
+    if @product.user_id == current_user.id || @product.order.present?
+      redirect_to root_path
+    end
+  end
+
+  def order_form_params
+    params.require(:order_form).permit(
+      :postal_code, :prefecture_id, :city, :house_number,
+      :building_name, :phone_number, :token
+    )
   end
 
 
