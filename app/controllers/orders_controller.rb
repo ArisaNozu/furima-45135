0df2b476d86c7cf02618886eb6a70fee75614f0f
ArisaNozu <myshkin88@gmail.com>
2025-07-19 11:52:06 +0900

    Ordersコントローラーを作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,13 @@
 class OrdersController < ApplicationController
+
+  def create
+  @order_form = OrderForm.new(order_form_params)
+
+  if @order_form.valid?
+    @order_form.save
+    redirect_to root_path
+  else
+    render :new
+  end
+
 end
