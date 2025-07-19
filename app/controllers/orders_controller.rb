4b8b5a3814fc8a8288e737a74e4fdc99f3af44ad
ArisaNozu <myshkin88@gmail.com>
2025-07-19 12:40:54 +0900

    ordersコントローラーのend漏れ修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,11 +3,12 @@ class OrdersController < ApplicationController
   def create
   @order_form = OrderForm.new(order_form_params)
 
-  if @order_form.valid?
-    @order_form.save
-    redirect_to root_path
-  else
-    render :new
+    if @order_form.valid?
+      @order_form.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
 end
