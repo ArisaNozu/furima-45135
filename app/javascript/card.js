8da0fe9bb00de5277b621c99a03ef18f3d902e46
ArisaNozu <myshkin88@gmail.com>
2025-07-21 14:10:24 +0900

     JavaScriptで公開鍵を読み込み

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,6 @@
 const pay = () => {
-  const payjp = Payjp('pk_test_92f88b5e311c383453af263b')// PAY.JPテスト公開鍵
+  const publicKey = gon.public_key
+  const payjp = Payjp(publicKey) // PAY.JPテスト公開鍵
   const elements = payjp.elements();
   const numberElement = elements.create('cardNumber');
   const expiryElement = elements.create('cardExpiry');
